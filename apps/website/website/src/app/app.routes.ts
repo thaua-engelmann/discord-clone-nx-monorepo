bcc257ca20a93020014a68d77448f9bf2513ec70
thaua-engelmann <engelmann.webdev@gmail.com>
2024-09-17 19:59:42 -0300

    feat(website): created home page and set up initial routing

--- apps/website/website/src/app/app.routes.ts
+++ apps/website/website/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+    }
+];
