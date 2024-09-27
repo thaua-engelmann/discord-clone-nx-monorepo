d8329c468ee0f375c5898cfd0a10d7cd63e40af8
thaua-engelmann <engelmann.webdev@gmail.com>
2024-09-26 22:33:26 -0300

    feat(header, navbar): created both components and added logo, links and a *Open Discord* btn.

--- apps/website/website/src/app/ui/navbar/navbar.component.ts
+++ apps/website/website/src/app/ui/navbar/navbar.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+type NavbarItem = {
+  id: number,
+  txt: string;
+}
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -8,4 +13,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  protected navbarItems: NavbarItem[] = [
+    {
+      id: 1,
+      txt: "Download"
+    },
+    {
+      id: 2,
+      txt: "Nitro"
+    },
+    {
+      id: 3,
+      txt: "Discover"
+    },
+    {
+      id: 4,
+      txt: "Safety"
+    },
+    {
+      id: 5,
+      txt: "Support"
+    },
+    {
+      id: 6,
+      txt: "Blog"
+    },
+    {
+      id: 7,
+      txt: "Careers"
+    },
+  ];
+
+}
