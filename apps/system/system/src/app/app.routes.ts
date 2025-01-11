9bbff37599d960252bc79a8b074e973dbbc30d44
thaua-engelmann <engelmann.webdev@gmail.com>
2025-01-11 00:17:24 -0300

    feat(register): created register component and configured its routing

--- apps/system/system/src/app/app.routes.ts
+++ apps/system/system/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/register/register.component').then(c => c.RegisterComponent),
+        pathMatch: 'full'
+    }
+];
