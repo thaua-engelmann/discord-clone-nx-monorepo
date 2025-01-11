3304d4911f1900fe1f32338b806d66088245fa8c
thaua-engelmann <engelmann.webdev@gmail.com>
2025-01-11 12:37:22 -0300

    feat(system, login): created formGroup

--- apps/system/system/src/app/pages/login/login.component.ts
+++ apps/system/system/src/app/pages/login/login.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  private formBuilder = inject(FormBuilder);
+  protected form: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
+
+  protected handleLogin = () => {
+
+    if (!this.form.valid) {
+      return alert('Form is not valid!');
+    }
+
+    alert(JSON.stringify(this.form.getRawValue()));
+  }
+}
